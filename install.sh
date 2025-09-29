194a895b152386ac850d0bf1749d48cf8477dd4a
Vladislav S <vlad.waxme@gmail.com>
2025-09-29 09:05:19 +0300

    simplify

--- install.sh
+++ install.sh
@@ -1,15 +1,11 @@
 dir=${1:-game-player-workspace}
-echo "installing to $dir..."
+echo "Installing to $dir..."
+rm -rf $dir
 mkdir -p $dir
 cd $dir
 git init
-git submodule add git@github.com:BrightSDKGamePlayer/Generator.git game-player/generator
+git submodule add git@github.com:BrightSDKGamePlayer/Generator.git game-player/apps
 git submodule add git@github.com:BrightSDKGamePlayer/Util-apps-generator-gulp.git util/apps-generator-gulp
-git submodule add git@github.com:BrightSDKGamePlayer/Util-app-image-processor.git util/app-image-processor
-git submodule add git@github.com:BrightSDKGamePlayer/Util-exec.git util/exec
-git submodule add git@github.com:BrightSDKGamePlayer/Util-logger.git util/logger
-git submodule add git@github.com:vladislavs-luminati/bright-sdk-integration.git util/bright-sdk-integration
-# @TODO: create releases dir in generator
-cd game-player/generator && mkdir releases && git submodule init && git submodule update && cd -
+git submodule update --init --recursive
+cd game-player/apps && git submodule init && git submodule update && npm install && cd -
 cd util && ls -d */ | xargs -I {} npm install {} && cd -
-cd game-player/generator && npm install && npm run install-local && cd -
