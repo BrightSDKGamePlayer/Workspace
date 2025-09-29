1e100869f7760c76c70b160e9e14a3cdde469646
Vladislav S <vlad.waxme@gmail.com>
2025-09-29 10:40:19 +0300

    include client

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd $dir
 git init
 git submodule add git@github.com:BrightSDKGamePlayer/Generator.git game-player/apps
 git submodule add git@github.com:BrightSDKGamePlayer/Util-apps-generator-gulp.git util/apps-generator-gulp
+git submodule add git@github.com:BrightSDKGamePlayer/WebClient client
 git submodule update --init --recursive
 cd game-player/apps && git submodule init && git submodule update && npm install && cd -
 cd util && ls -d */ | xargs -I {} npm install {} && cd -
